45cd30c39bdd22c05d5801e6343955187e9c6360
yu liu <yuliu@yus-MacBook-Pro.local>
2018-03-12 14:57:31 -0700

    no train

--- backend_launcher.sh
+++ backend_launcher.sh
@@ -11,9 +11,9 @@ cd ./backend_server
 python2 service.py &
 cd ../news_recommendation_service
 python2 click_log_processor.py &
-python2 recommendation_service.py &
-cd ../news_topic_modeling_service/server
-python2 server.py &
+python2 recommendation_service.py 
+# cd ../news_topic_modeling_service/server
+# python2 server.py &
 
 echo“====================================”
 read -p "PRESS [ENTER] TO TERMINATE PROCESSES" PRESSKEY
